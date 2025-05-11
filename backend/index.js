6b195f5eed50d8ea630258d9c47fa8baf0dbc0a9
MithileshwaranS <mithileshwaran24@gmail.com>
2025-05-11 11:02:55 +0530

    changed build to dist fix

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.get('/api/yarnCounts', async (req, res) => {
 app.use('/myapp', express.static(path.join(__dirname, 'frontend', 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 // POST: Submit costing
